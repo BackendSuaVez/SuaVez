b7d07b7a956a1b9838ccec2e47f93a2a1d6171f9
Caroline <carolineevelyn08aguiar@gmail.com>
2026-02-24 00:31:34 -0300

    cliente(nao terminado)

---
+++ src/main/java/com/example/suavez/entity/Cliente.java
@@ -0,0 +1,23 @@
+package com.example.suavez.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Cliente {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String cpf;
+    private String email;
+    private String senha;
+    private String data_cadastro;
+
+}
