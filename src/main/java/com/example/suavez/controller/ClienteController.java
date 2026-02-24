c29d0dab9876741f7c7b5c84def31ab4c9ee9248
Caroline <carolineevelyn08aguiar@gmail.com>
2026-02-24 00:38:12 -0300

    cliente(nao terminado)

---
+++ src/main/java/com/example/suavez/controller/ClienteController.java
@@ -0,0 +1,4 @@
+package com.example.suavez.controller;
+
+public class ClienteController {
+}
--- src/main/java/com/example/suavez/entity/Cliente.java
+++ src/main/java/com/example/suavez/entity/Cliente.java
@@ -19,4 +19,60 @@ public class Cliente {
     private String email;
     private String senha;
     private String data_cadastro;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getData_cadastro() {
+        return data_cadastro;
+    }
+
+    public void setData_cadastro(String data_cadastro) {
+        this.data_cadastro = data_cadastro;
+    }
 }
