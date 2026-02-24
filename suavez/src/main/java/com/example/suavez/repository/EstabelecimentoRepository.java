c46e53e43518b6c9c897dddbfc2e8f3213aadbdd
Caroline <carolineevelyn08aguiar@gmail.com>
2026-02-24 00:05:15 -0300

    ajuste no projeto

--- suavez/src/main/java/com/example/suavez/repository/EstabelecimentoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.suavez.repository;
-
-import com.example.suavez.entity.Estabelecimento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, Long> {
-}
