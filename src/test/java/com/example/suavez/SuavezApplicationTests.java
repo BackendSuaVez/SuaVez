056a3d01a775a2f3614ee7cd57761d21d2a41f3d
Caroline <carolineevelyn08aguiar@gmail.com>
2026-02-24 00:08:41 -0300

    ajuste no projeto

---
+++ src/test/java/com/example/suavez/SuavezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suavez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuavezApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
